411fcee78df847936f52a0068c2c2f6084971f7a
Alex Ehlke <alex.ehlke@gmail.com>
2024-12-08 00:59:53 -0500

    utf8 optimizations wip
    # Conflicts:
    #	Sources/HtmlTreeBuilder.swift
    #	Sources/TreeBuilder.swift

--- Sources/SwiftSoup.swift
+++ Sources/SwiftSoup.swift
@@ -191,7 +191,7 @@ import Foundation
 	
 	@see Cleaner#clean(Document)
 	*/
-	public  func clean(_ bodyHtml: String, _ baseUri: String, _ whitelist: Whitelist)throws->String? {
+	public  func clean(_ bodyHtml: String, _ baseUri: String, _ whitelist: Whitelist) throws -> String? {
 		let dirty: Document = try parseBodyFragment(bodyHtml, baseUri)
 		let cleaner: Cleaner = Cleaner(whitelist)
 		let clean: Document = try cleaner.clean(dirty)
@@ -208,7 +208,7 @@ import Foundation
 	
 	@see Cleaner#clean(Document)
 	*/
-	public  func clean(_ bodyHtml: String, _ whitelist: Whitelist)throws->String? {
+	public  func clean(_ bodyHtml: String, _ whitelist: Whitelist) throws -> String? {
 		return try SwiftSoup.clean(bodyHtml, "", whitelist)
 	}
 
