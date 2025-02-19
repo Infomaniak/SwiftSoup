130d5ace6deeb3795b8917cba57e9128ff77a236
Alex Ehlke <alex.ehlke@gmail.com>
2025-02-18 23:31:08 -0500

    wip

--- Sources/Element.swift
+++ Sources/Element.swift
@@ -351,8 +351,8 @@ open class Element: Node {
     public func appendChild(_ child: Node) throws -> Element {
         // was - Node#addChildren(child). short-circuits an array create and a loop.
         try reparentChild(child)
-        ensureChildNodes()
         childNodes.append(child)
+        updateQueryIndex(for: [child], adding: true)
         child.setSiblingIndex(childNodes.count - 1)
         return self
     }
@@ -531,6 +531,7 @@ open class Element: Node {
      */
     @discardableResult
     public func empty() -> Element {
+        updateQueryIndex(for: childNodes, adding: false)
         childNodes.removeAll()
         return self
     }
