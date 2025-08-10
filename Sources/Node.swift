0c4cf1563eb14b848a7770489e88cafb2e44eb70
Marc Haisenko <haisenko@equinux.com>
2025-08-10 13:21:17 +0200

    Improve some array mappings and filterings

--- Sources/Node.swift
+++ Sources/Node.swift
@@ -341,11 +341,7 @@ open class Node: Equatable, Hashable, @unchecked Sendable {
      */
     @inline(__always)
     open func childNodesCopy() -> Array<Node> {
-        var children: Array<Node> = Array<Node>()
-        for node: Node in childNodes {
-            children.append(node.copy() as! Node)
-        }
-        return children
+        return childNodes.map { $0.copy() as! Node }
     }
     
     /**
@@ -705,10 +701,9 @@ open class Node: Equatable, Hashable, @unchecked Sendable {
         
         let nodes: Array<Node> = parentNode!.childNodes
         var siblings: Array<Node> = Array<Node>()
-        for node in nodes {
-            if (node !== self) {
-                siblings.append(node)
-            }
+        siblings.reserveCapacity(nodes.count - 1)
+        for node in nodes where node !== self {
+            siblings.append(node)
         }
         
         return siblings
@@ -905,11 +900,7 @@ open class Node: Equatable, Hashable, @unchecked Sendable {
         clone.attributes = attributes != nil ? attributes?.clone() : nil
         clone.attributes?.ownerElement = clone as? SwiftSoup.Element
         clone.baseUri = baseUri
-        clone.childNodes = Array<Node>()
-        
-        for child in childNodes {
-            clone.childNodes.append(child)
-        }
+        clone.childNodes = childNodes
         
         if let cloneElement = clone as? Element {
             cloneElement.rebuildQueryIndexesForThisNodeOnly()
