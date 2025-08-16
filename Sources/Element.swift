6a3121c7d923d78fdc46e4a382248d7b7df41da5
Marc Haisenko <haisenko@equinux.com>
2025-08-16 14:21:20 +0200

    Fix :last-child selector
    The evaluator was mixing element and node indices. Simply check whether the element has a next element sibling. Also don't match the root node.

--- Sources/Element.swift
+++ Sources/Element.swift
@@ -682,7 +682,8 @@ open class Element: Node {
             if (siblings.count > index!+1) {
                 return siblings[index!+1]
             } else {
-                return nil}
+                return nil
+            }
         }
         return nil
     }
